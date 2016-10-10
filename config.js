7ef18e7883d1da49728946320c6331b76ff6a78a
shellbell22 <morris.shelley@gmail.com>
2016-10-10 16:12:31 -0400

    travis continuous deployment

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
                             'mongodb://localhost/shopping-list' :
                             'mongodb://localhost/shopping-list-dev'
                           );
-                          //'mongodb://testuser1:TestUserPassword1@ds053206.mlab.com:53206/shellbell22shoppinglist)';
+                    
 exports.PORT = process.env.PORT || 8080;
